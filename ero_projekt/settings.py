f9874479a13e7f243268decb43bae8d0e74b0eab
pinky84 <prafaelic@veleri.hr>
2021-02-19 13:36:28 +0100

    projekt povezan sa mysql bazom erodb

--- ero_projekt/settings.py
+++ ero_projekt/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'ero_projekt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'erodb',
+        'USER': 'admin',
+        'PASSWORD': 'Pinky_11',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
