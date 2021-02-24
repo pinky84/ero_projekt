81c40e36afaabd0c812631324cdee77c7f0cd7cb
pinky84 <prafaelic@veleri.hr>
2021-02-24 21:31:01 +0100

    Lokacija template: detail i view

--- eroapp/urls.py
+++ eroapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import *
 
 
 urlpatterns = [
     path('', views.home),
-    path('uredjaji/', views.uredjaji),
+    path('lokacije/', views.lokacija_list),
+    path('lokacije/<pk>/', views.lokacija_detail),
+
+    #path('lokacije/', LokacijaListView.as_view(), name='lokacija_list_view'),
 ]
--- eroapp/views.py
+++ eroapp/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
-
+"""
 def uredjaji(request):
     return render(request, 'uredjaji.html')
+"""
+
+"""class LokacijaListView(ListView):
+    template_name   = 'lokacija/lokacija_list_view.html'
+    queryset        = Lokacija.objects.all()
+    context_object_name = 'lokacije'
+"""
+
+def lokacija_list(request):
+    lokacije = Lokacija.objects.all()
+    context = {'lokacije': lokacije}
+    return render(request, 'lokacija/lokacija_list_view.html', context)
+
+def lokacija_detail(request, pk):
+    lokacija = Lokacija.objects.get(id=pk)
+    context = {'lokacija': lokacija}
+    return render(request, 'lokacija/lokacija_detail.html', context)
+
+#------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 
